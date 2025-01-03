4a1e211a8b6bf53c8eb8cfd0a2edd6399985f4f2
FedoRich-P <panchev_fedor@mail.ru>
2025-01-04 02:16:45 +0300

    lesson 73

--- src/layout/profile/ProfileStatus.tsx
+++ src/layout/profile/ProfileStatus.tsx
@@ -1,34 +1,40 @@
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, useEffect, useState} from "react";
 import {useAppSelector} from "../../common/hooks/useAppSelector.ts";
 import {selectProfile} from "../../app/appSelectors.ts";
 import {useAppDispatch} from "../../common/hooks/useAppDispatch.ts";
-import { UpdateProfileStatusTC} from "../../redux/profileReducer.ts";
+import {UpdateProfileStatusTC} from "../../redux/profileReducer.ts";
 
 export const ProfileStatus = () => {
     const [editMode, setEditMode] = useState(false)
+    const [value, setValue] = useState('')
+    const [valueToState, setValueToState] = useState('')
     const status = useAppSelector(selectProfile).status;
     const dispatch = useAppDispatch()
 
-     const activateEditMode = () => {
+    useEffect(() => {
+        dispatch(UpdateProfileStatusTC({status: valueToState}))
+    }, [valueToState])
+
+    const activateEditMode = () => {
         setEditMode(editMode => !editMode)
-     }
+        setValueToState(value)
+    }
 
-     const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-         dispatch(UpdateProfileStatusTC({status: e.currentTarget.value}))
-     }
-
+    const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value)
+    }
 
     return (
         <>
-            { !editMode
-                    ?
-                    <div className="">
-                        <h3 onDoubleClick={activateEditMode}>{status || "Статуса пока нет..."}</h3>
-                    </div>
-                    :
-                    <div className="">
-                        <input onBlur={activateEditMode} onInput={onInputChange} autoFocus type="text"/>
-                    </div>
+            {!editMode
+                ?
+                <div className="">
+                    <h3 onDoubleClick={activateEditMode}>{status || "Статуса пока нет..."}</h3>
+                </div>
+                :
+                <div className="">
+                    <input value={value} onBlur={activateEditMode} onInput={onInputChange} autoFocus type="text"/>
+                </div>
             }
         </>
     );
